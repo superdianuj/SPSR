774c06ce6b008ea2b89253e7bde2dbc71128b347
Spider Man <dianujkotov15@gmail.com>
2024-05-31 06:10:00 +0900

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+python code/colmap_runner.py --path processed_results SR_pred_colmap
+python code/visualizer.py --dir processed_results
 cd code
 python test.py -opt options/test/test_spsr.json
 cd ..
 python process_imgs.py
+python code/colmap_runner.py --path processed_results SR_pred_colmap
+python code/visualizer.py --dir processed_results
